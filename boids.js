905215b0c8402b6b914e55230521bbc7fd4c4906
Cliff <cliffyyc@outlook.com>
2021-07-18 06:54:53 -0400

    Leader actions, follow() and constructor changes

--- boids.js
+++ boids.js
@@ -1,5 +1,5 @@
 var boids = [];
-var boidCount = 2;
+var boidCount = 150;
 
 /*
 const width = window.innerWidth;
@@ -19,6 +19,7 @@ class Boid {
   constructor() {
     this.size = 1;
     this.color = "#558cf4";
+
     this.x = Math.random() * width;
     this.y = Math.random() * height;
     this.vX = Math.random() * 6 - 3;
@@ -27,14 +28,17 @@ class Boid {
     this.direction = Math.atan2(this.vY, this.vX);
     this.aX = Math.random() * 8 - 4;
     this.aY = Math.random() * 8 - 4;
-    this.maxSpeed = 4;
+    this.maxSpeed = 3;
     this.maxForce = 0.25;
-    this.influence = 1;
-    this.influenceRange = 100;
-    this.actionRange = 100;
 
     this.wanderCheck = false;
     this.wanderAngle = Math.random() * Math.PI * 2;
+
+    this.group = 1;
+    this.leader = false;
+    this.influence = 1;
+    this.influenceRange = 100;
+    this.actionRange = 100;
   }
 
   draw(ctx) {
@@ -46,29 +50,15 @@ class Boid {
     ctx.translate(-canvasX, -canvasY);
     ctx.fillStyle = this.color;
 
+    let size = Math.pow(this.size, 0.3);
     ctx.beginPath();
     ctx.moveTo(canvasX, canvasY);
-    ctx.lineTo(canvasX - 15, canvasY + 5);
-    ctx.lineTo(canvasX - 15, canvasY - 5);
+    ctx.lineTo(canvasX - 13.5*size, canvasY + 4.5*size);
+    ctx.lineTo(canvasX - 13.5*size, canvasY - 4.5*size);
     ctx.lineTo(canvasX, canvasY);
     
     ctx.fill();
     ctx.setTransform(1, 0, 0, 1, 0, 0);
-  }
-
-  bound() {
-    if (this.x > width) {
-      this.x -= width;
-    }
-    if (this.x < 0) {
-      this.x += width;
-    }
-    if (this.y > height) {
-      this.y -= height;
-    }
-    if (this.y < 0) {
-      this.y += height;
-    }
   }
   
   limitAcceleration() {
@@ -98,9 +88,11 @@ class Boid {
     if (this.vX != 0 && this.vY != 0) {
       this.direction = Math.atan2(this.vY, this.vX);
     }
+
+    [this.x, this.y] = bound(this.x, this.y);
   }
 
-  seekFlee(targetX, targetY, mode=1) {
+  seekFlee(targetX, targetY, mode=1, coeff=1) {
     // This changes the orientation of the velocity vector but not the magnitude
     // mode is 1 for seeking and -1 for fleeing
     let desiredX = targetX - this.x;
@@ -109,27 +101,25 @@ class Boid {
     
     // the desired values are negated compared to original algorithm, 
     // thus the (steering = desired - current) step should be skipped
-    this.aX += (desiredX * (this.v / dist) * mode);
-    this.aY += (desiredY * (this.v / dist) * mode);
+    this.aX += (desiredX * (this.v / dist) * mode) * coeff;
+    this.aY += (desiredY * (this.v / dist) * mode) * coeff;
   }
 
   mouseSeekFlee(mode=1) {
     if (Math.sqrt((this.x - mouseX)**2 + (this.y - mouseY)**2) < this.actionRange) {
-      this.seekFlee(mouseX, mouseY, mode=mode);
-      this.seekFlee(mouseX, mouseY, mode=mode);
-      this.seekFlee(mouseX, mouseY, mode=mode);
+      this.seekFlee(mouseX, mouseY, mode, 3);
     }
   }
 
-  arrive(targetX, targetY) {
+  arrive(targetX, targetY, coeff=1) {
     let desiredX = targetX - this.x;
     let desiredY = targetY - this.y;
     let dist = Math.sqrt(desiredX**2 + desiredY**2);
 
-    let coeff = Math.min(dist / this.actionRange, 1) ** 0.75;
-    if (coeff > 0.01) {
-      this.aX += ((desiredX * (coeff / dist) * this.maxSpeed) - this.vX);
-      this.aY += ((desiredY * (coeff / dist) * this.maxSpeed) - this.vY);
+    let c = Math.min(dist / this.actionRange, 1) ** 0.75;
+    if (c > 0.01) {
+      this.aX += ((desiredX * (c / dist) * this.maxSpeed) - this.vX) * coeff;
+      this.aY += ((desiredY * (c / dist) * this.maxSpeed) - this.vY) * coeff;
     } else {
       this.vX = 0;
       this.vY = 0;
@@ -154,6 +144,8 @@ class Boid {
   separate(group) {
     let steeringX = 0;
     let steeringY = 0;
+    let count = 0
+    let countInf = 0;
 
     for (let each of group) {
       let distX = each.x - this.x;
@@ -161,14 +153,17 @@ class Boid {
       let dist = Math.sqrt(distX**2 + distY**2);
 
       if (0 < dist && dist < this.actionRange) {
-        steeringX += (distX / Math.sqrt(distX**2 + distY**2));
-        steeringY += (distY / Math.sqrt(distX**2 + distY**2));
+        count += 1;
+        countInf += each.influence;
+        steeringX += (distX / Math.sqrt(distX**2 + distY**2)) * each.influence;
+        steeringY += (distY / Math.sqrt(distX**2 + distY**2)) * each.influence;
       }
-      
     }
 
-    this.aX += (this.vX - steeringX);
-    this.aY += (this.vY - steeringY);
+    if (count != 0) {
+      this.aX += (this.vX - steeringX*count/countInf);
+      this.aY += (this.vY - steeringY*count/countInf);
+    }
   }
 
   cohere(group) {
@@ -177,14 +172,16 @@ class Boid {
     let count = 0;
 
     for (let each of group) {
-      let distX = each.x - this.x;
-      let distY = each.y - this.y;
-      let dist = Math.sqrt(distX**2 + distY**2);
+      if (each.leader == false && each.group == this.group){
+        let distX = each.x - this.x;
+        let distY = each.y - this.y;
+        let dist = Math.sqrt(distX**2 + distY**2);
 
-      if (0 < dist && dist < this.actionRange) {
-        count += each.influence;
-        avgX += each.x * each.influence;
-        avgY += each.y * each.influence;
+        if (0 < dist && dist < this.actionRange) {
+          count += each.influence;
+          avgX += each.x * each.influence;
+          avgY += each.y * each.influence;
+        }
       }
     }
 
@@ -199,15 +196,17 @@ class Boid {
     let count = 0;
 
     for (let each of group) {
-      let distX = each.x - this.x;
-      let distY = each.y - this.y;
-      let dist = Math.sqrt(distX**2 + distY**2);
+      if (each.group == this.group) {
+        let distX = each.x - this.x;
+        let distY = each.y - this.y;
+        let dist = Math.sqrt(distX**2 + distY**2);
 
-      if (0 < dist && dist < this.actionRange) {
-        count += each.influence;
-        avgvX += each.vX * each.influence;
-        avgvY += each.vY * each.influence;
-      }
+        if (0 < dist && dist < this.actionRange) {
+          count += each.influence;
+          avgvX += each.vX * each.influence;
+          avgvY += each.vY * each.influence;
+        }
+      } 
     }
 
     if (count != 0) {
@@ -218,7 +217,7 @@ class Boid {
 
   wander() {
     if (this.wanderCheck == true) {
-      this.wanderAngle += (Math.random()*0.5 - 0.25);
+      this.wanderAngle += (Math.random()*0.4 - 0.2);
 
       if (this.wanderAngle < -Math.PI) {
         this.wanderAngle += Math.PI;
@@ -233,9 +232,27 @@ class Boid {
   }
 
   follow(leader) {
-    let dist = pointLineDist(this.x, this.y, leader.x, leader.y, leader.x + leader.vX*40, leader.y + leader.vY*40);
-    console.log(leader.x-this.x, dist)
+    let pathDist = pointLineDist(this.x, this.y, leader.x, leader.y, 
+      leader.x + leader.vX*leader.influence*3, leader.y + leader.vY*leader.influence*3);
 
+    if (pathDist != false && pathDist[0] < leader.influence*4) {
+      this.color = "#000000";
+      this.seekFlee(pathDist[1], pathDist[2], -1, leader.influence);
+    } else {
+      this.color = "#558cf4";
+    }
+
+    let dist1 = Math.sqrt((this.x - leader.x)**2 + (this.y - leader.y)**2);
+    let posX = leader.x - leader.vX*leader.influence*2;
+    let posY = leader.y - leader.vY*leader.influence*2;
+    [posX, posY] = bound(posX, posY);
+    let dist2 = Math.sqrt((this.x - posX)**2 + (this.y - posY)**2);
+
+    if (dist1 < this.actionRange) {
+      this.seekFlee(leader.x + leader.vX*leader.influence*2, leader.y + leader.vY*leader.influence*2, 1, leader.influence);
+    } else if (dist2 < this.actionRange) {
+      this.seekFlee(posX+leader.vX*leader.influence*2, posY+leader.vY*leader.influence*2, 1, leader.influence);
+    }
   }
 }
 
@@ -243,7 +260,7 @@ class Boid {
 function pointLineDist(pX, pY, lX1, lY1, lX2, lY2) {
   // just in case the length of the line is 0
   if (lX1 == lX2 && lY1 == lY2) {
-    return Math.sqrt((pX - lX1)**2 + (pY - lY1)**2);
+    return false;
   }
 
   let p1 = (pX - lX1)*(lX2 - lX1) + (pY - lY1)*(lY2 - lY1);
@@ -251,15 +268,32 @@ function pointLineDist(pX, pY, lX1, lY1, lX2, lY2) {
   let p = p1 / p2;
 
   if (p < 0) {
-    console.log('p < 0', p);
-    return Math.sqrt((pX - lX1)**2 + (pY - lY1)**2);
+    //return Math.sqrt((pX - lX1)**2 + (pY - lY1)**2);
+    return false;
   } else if (p > 1) {
-    console.log('p > 1', p);
-    return Math.sqrt((pX - lX2)**2 + (pY - lY1)**2);
+    //return Math.sqrt((pX - lX2)**2 + (pY - lY1)**2);
+    return false;
   } else {
-    console.log('0 < p < 1', p);
-    return Math.sqrt((pX - (lX1 + p * (lX2 - lX1)))**2 + (pY - (lY1 + p * (lY2 - lY1)))**2);
+    let posX = lX1 + p * (lX2 - lX1);
+    let posY = lY1 + p * (lY2 - lY1);
+    return [Math.sqrt((pX - posX)**2 + (pY - posY)**2), posX, posY];
   }
+}
+
+
+function bound(x, y) {
+  if (x > width) {
+    x -= width;
+  } else if (x < 0) {
+    x += width;
+  }
+  if (y > height) {
+    y -= height;
+  } else if (y < 0) {
+    y += height;
+  }
+
+  return [x, y];
 }
 
 
@@ -289,7 +323,18 @@ function testChange() {
 
 
 function nextFrame() {
-  boids[1].follow(boids[0]);
+  for (let i=1; i<boidCount; i++) {
+    boids[i].follow(boids[0]);
+
+    boids[i].separate(boids);
+    boids[i].cohere(boids);
+    boids[i].align(boids);
+    boids[i].mouseSeekFlee(mode=test);
+  }
+
+  boids[0].wander();
+
+
   for (let b of boids) {
     /*
     b.separate(boids);
@@ -301,7 +346,6 @@ function nextFrame() {
 
     b.limitAcceleration();
     b.update();
-    b.bound();
   }
 
   const ctx = document.getElementById("Boids").getContext("2d");
@@ -325,19 +369,11 @@ window.onload = () => {
 
   spawnBoids();
   boids[0].color = "#ff0000";
-
-  boids[0].x = 200;
-  boids[0].y = 400;
-  boids[0].vX = 1;
-  boids[0].vY = 0;
-  boids[0].aX = 0;
-  boids[0].aY = 0;
-  boids[1].x = 500;
-  boids[1].y = 400;
-  boids[1].vX = 0;
-  boids[1].vY = 0;
-  boids[1].aX = 0;
-  boids[1].aY = 0;
+  boids[0].size = 4;
+  boids[0].wanderCheck = true;
+  boids[0].influence = 20;
+  boids[0].maxForce = 0.15;
+  boids[0].leader = true;
 
   var mouse = document.querySelector("#Boids");
   mouse.addEventListener("mousemove", updateMouse, false);
